453eaac6562acaef66ecf77b484f795fc5893f99
lxq <lxq@weetgo.com>
2019-07-29 19:25:01 +0800

    cargo new --bin

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+/**
+ * My learning of rust.
+ * @author Allen Lin
+ * @email xqlin@qq.com
+ * @date 2019/7/27
+ */
+
+
+fn main() {
+    println!("你好, Rust!");
+    let a = 'b';
+    let s = "未高科技";
+    println!("{}, {}",a, s);
+}
